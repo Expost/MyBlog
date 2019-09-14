80d1d11345df7f1c3512e741485a6380a0093370
expost <tpxkldh@163.com>
2019-09-14 13:12:14 +0800

    更新blog

--- create.bat
+++ create.bat
@@ -1,3 +1,4 @@
+@echo off
 rd /S /Q public
 hugo
 cd public
@@ -5,4 +6,11 @@ git init
 git add -A
 git commit -m "update blog"
 git push -f git@github.com:Expost/expost.github.io.git master
+
+echo 是否要更新hugo blog到git仓库上？是则回车，否则关闭窗口
 pause
+
+cd ..
+git add *
+git commit -m "更新blog"
+git push origin master
